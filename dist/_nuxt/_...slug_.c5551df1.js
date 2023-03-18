ba8a8ea88439360fe6e4f54a9b37e0cfbc324d50
miaoyu <miaoyu2009@qq.com>
2023-03-18 22:01:51 +0800

    remove dist

--- dist/_nuxt/_...slug_.c5551df1.js
+++
@@ -1,1 +0,0 @@
-import{a as e,b as a}from"./entry.eed129ac.js";const s=e({__name:"[...slug]",setup(n){return a("/zh/home"),(t,o)=>null}});export{s as default};
--- dist/_nuxt/composables.e76b84ea.js
+++
@@ -1,1 +0,0 @@
-import{E as r}from"./entry.eed129ac.js";function t(e,u){return r()._useHead(e,u)}export{t as u};
