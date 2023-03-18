c4ad2301f0eef9451ef4fc6be880af47233ad919
miaoyu <miaoyu2009@qq.com>
2023-03-18 21:56:21 +0800

    dist

---
+++ dist/_nuxt/_...slug_.c5551df1.js
@@ -0,0 +1,1 @@
+import{a as e,b as a}from"./entry.eed129ac.js";const s=e({__name:"[...slug]",setup(n){return a("/zh/home"),(t,o)=>null}});export{s as default};
---
+++ dist/_nuxt/composables.e76b84ea.js
@@ -0,0 +1,1 @@
+import{E as r}from"./entry.eed129ac.js";function t(e,u){return r()._useHead(e,u)}export{t as u};
